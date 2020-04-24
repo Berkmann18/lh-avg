97e7d54c2a680ad33f16065aea68ec1d031e2d52
Berkmann18 <maxieberkmann@gmail.com>
2020-04-24 20:05:39 +0100

    test(index): added percentage-related tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -14,20 +14,37 @@ describe('baseline', () => {
     expect(avg('0/0/0/0/(0/1, 0/1, 0/2)')).toBe(0);
   });
 
+  test('full 0% (longhand)', () => {
+    expect(avg('0/0/0/0/(0/1, 0/1, 0/2)', true)).toBe('0%');
+  });
+
   test('full 100 (longhand)', () => {
     expect(avg('100/100/100/100/(1/1, 1/1, 2/2)')).toBe(1);
+  });
+
+  test('full 100% (longhand)', () => {
+    expect(avg('100/100/100/100/(1/1, 1/1, 2/2)', true)).toBe('100%');
   });
 
   test('full 0', () => {
     expect(avg('0/0/0/0/(0, 0, 0)')).toBe(0);
   });
 
+  test('full 0%', () => {
+    expect(avg('0/0/0/0/(0, 0, 0)', true)).toBe('0%');
+  });
+
   test('full 100', () => {
     expect(avg('100/100/100/100/(3, 3, 7)')).toBe(1);
+  });
+
+  test('full 100%', () => {
+    expect(avg('100/100/100/100/(3, 3, 7)', true)).toBe('100%');
   });
 });
 
 const result = 0.423673469387755;
+const perc = '42.37%';
 
 describe('normal', () => {
   test('longhand', () => {
@@ -40,6 +57,20 @@ describe('normal', () => {
 
   test('shorthand (alt)', () => {
     expect(avg('13 / 94 / 86 / 75 / (0/ 0/ 2)')).toStrictEqual(result);
+  });
+});
+
+describe('percentage', () => {
+  test('longhand', () => {
+    expect(avg('13 / 94 / 86 / 75 / (0/3, 0/3, 2/7)', true)).toStrictEqual(perc);
+  });
+
+  test('shorthand', () => {
+    expect(avg('13 / 94 / 86 / 75 / (0, 0, 2)', true)).toStrictEqual(perc);
+  });
+
+  test('shorthand (alt)', () => {
+    expect(avg('13 / 94 / 86 / 75 / (0/ 0/ 2)', true)).toStrictEqual(perc);
   });
 });
 
