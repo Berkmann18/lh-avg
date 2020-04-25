e94058abae70d3dbf41651db960fcef000e8cb3f
Berkmann18 <maxieberkmann@gmail.com>
2020-04-25 17:01:07 +0100

    docs(index): added a doclet

--- src/index.ts
+++ src/index.ts
@@ -25,6 +25,47 @@ interface Result {
   average: NumLike;
 }
 
+/**
+ * Lighthouse average score calculator.
+ * @param {string} scoreStr Lighthouse score string of the form ('num / num / num / num / (num, num, num)')
+ * @param {boolean} [asPercentage=false] Return percentages (_as strings_) instead of numbers
+ * @returns {Result} Result with the individual scores for each metrics.
+ * @example <caption>Spaced</caption>
+ * average('14 / 100 / 98 / 100 / (1, 0, 6)');
+ * // Returns:
+ * {
+ *   perf: 0.14,
+ *   a11y: 1,
+ *   bp: 0.98,
+ *   seo: 1,
+ *   pwa: { fnr: 0.3333333333333333, ins: 0, po: 0.8571428571428571 },
+ *   average: 0.6157823129251702
+ * }
+ * @example <caption>Show percentages</caption>
+ * average('14 / 100 / 98 / 100 / (1, 0, 6)', true);
+ * // Returns:
+ * {
+ *   perf: '14%',
+ *   a11y: '100%',
+ *   bp: '98%',
+ *   seo: '100%',
+ *   pwa: { fnr: '33.33%', ins: '0%', po: '85.71%' },
+ *   average: '61.58%'
+ * }
+ * @example <caption>Compressed</caption>
+ * average('14/100/98/100/(1,0,6)');
+ * // Returns:
+ * {
+ *   perf: 0.14,
+ *   a11y: 1,
+ *   bp: 0.98,
+ *   seo: 1,
+ *   pwa: { fnr: 0.3333333333333333, ins: 0, po: 0.8571428571428571 },
+ *   average: 0.6157823129251702
+ * }
+ *
+ * For more ways to input scores, please see the unit tests in __tests__/
+ */
 const avg = (scoreStr: string, asPercentage = false): Result => {
   const { output, shorthandForm } = split(scoreStr);
 
