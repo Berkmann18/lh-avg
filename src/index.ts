4c20c2038a31a727d41c5e0dbfff3b198bc68b7a
Berkmann18 <maxieberkmann@gmail.com>
2020-04-28 00:25:58 +0100

    refactor: less tech debts

--- src/index.ts
+++ src/index.ts
@@ -1,42 +1,10 @@
 // eslint-disable-next-line node/no-missing-import
-import split from './split';
+import { parseScoreString, percentagify, processDifference } from './process';
 
 /**
  * @private
  */
 const mean = (nums: number[]): number => nums.reduce((acc, val) => acc + val, 0) / nums.length;
-
-/**
- * @private
- */
-const perc = (num: number): string => `${Math.round(num * 10000) / 100}%`;
-
-/**
- * @private
- */
-const safeFractionEval = (fraction: string): number => {
-  // return fraction.split('/').map(parseFloat).reduce((acc, val) => acc / val);
-  const [num, denum] = fraction.split('/');
-  return parseInt(num) / parseInt(denum);
-};
-
-type NumLike = number | string;
-type SubObj = Record<string, NumLike>;
-type ResultKey = NumLike | SubObj;
-
-interface Result {
-  perf: NumLike; // Performance
-  a11y: NumLike; // Accessibility
-  bp: NumLike; // Best Practices
-  seo: NumLike; // Search Engine Optimization
-  pwa: {
-    fnr: NumLike; // Fast & Reliable
-    ins: NumLike; // Installable
-    po: NumLike; // PWA Optimized
-  };
-  average: NumLike;
-  [key: string]: ResultKey;
-}
 
 /**
  * Lighthouse average score calculator.
@@ -45,28 +13,9 @@ interface Result {
  * @returns {Result} Result with the individual scores for each metrics.
  * @private
  */
-const avg = (scoreStr: string, asPercentage = false): Result => {
-  const { output, shorthandForm } = split(scoreStr);
-
-  const perf = parseFloat(output.perf) / 100;
-  const a11y = parseFloat(output.a11y) / 100;
-  const bp = parseFloat(output.bp) / 100;
-  const seo = parseFloat(output.seo) / 100;
-  let fnr = null;
-  let ins = null;
-  let po = null;
-
-  if (shorthandForm) {
-    fnr = parseFloat(output.fnr) / 3;
-    ins = parseFloat(output.ins) / 3;
-    po = parseFloat(output.po) / 7;
-  } else {
-    fnr = safeFractionEval(output.fnr);
-    ins = safeFractionEval(output.ins);
-    po = safeFractionEval(output.po);
-  }
-
-  const scores = [perf, a11y, bp, seo, fnr, ins, po];
+const avg = (scoreStr: string, asPercentage: boolean): Result => {
+  const scores = parseScoreString(scoreStr);
+  const [perf, a11y, bp, seo, fnr, ins, po] = scores;
 
   const average = mean(scores);
   const out = {
@@ -82,20 +31,7 @@ const avg = (scoreStr: string, asPercentage = false): Result => {
     average
   };
 
-  if (asPercentage) {
-    return {
-      perf: perc(perf),
-      a11y: perc(a11y),
-      bp: perc(bp),
-      seo: perc(seo),
-      pwa: {
-        fnr: perc(fnr),
-        ins: perc(ins),
-        po: perc(po)
-      },
-      average: perc(average)
-    };
-  }
+  if (asPercentage) return percentagify(out);
 
   return out;
 };
@@ -189,41 +125,7 @@ const average = (
     });
   }
   if (showDiff) {
-    const ref = results[0];
-    const refPwa = ref.pwa as SubObj;
-    /* eslint-disable security/detect-object-injection */
-    if (asPercentage) {
-      for (let idx = 1; idx < scoreStrings.length; ++idx) {
-        ['perf', 'a11y', 'bp', 'seo', 'average'].forEach((key) => {
-          results[idx][key] =
-            parseFloat(results[idx][key] as string) - parseFloat(ref[key] as string);
-          const better = results[idx][key] > 0;
-          const noChange = results[idx][key] === 0;
-          results[idx][key] = noChange
-            ? '-'
-            : `${Math.round((results[idx][key] as number) * 100) / 100}%`;
-          if (better) results[idx][key] = '+' + results[idx][key];
-        });
-        const pwa = results[idx].pwa as SubObj;
-        ['fnr', 'ins', 'po'].forEach((key) => {
-          pwa[key] = parseFloat(pwa[key] as string) - parseFloat(refPwa[key] as string);
-          const better = pwa[key] > 0;
-          const noChange = pwa[key] === 0;
-          pwa[key] = noChange ? '-' : `${Math.round((pwa[key] as number) * 100) / 100}%`;
-          if (better) pwa[key] = '+' + pwa[key];
-        });
-      }
-      return results;
-    }
-    for (let idx = 1; idx < scoreStrings.length; ++idx) {
-      ['perf', 'a11y', 'bp', 'seo', 'average'].forEach((key) => {
-        (results[idx][key] as number) -= ref[key] as number;
-      });
-      const pwa = results[idx].pwa as SubObj;
-      ['fnr', 'ins', 'po'].forEach((key) => {
-        (pwa[key] as number) -= refPwa[key] as number;
-      });
-    }
+    processDifference(scoreStrings, results, asPercentage);
   }
 
   return results;
---
+++ src/process.ts
@@ -0,0 +1,108 @@
+// eslint-disable-next-line node/no-missing-import
+import split from './split';
+
+const signsResult = (result: number): string => {
+  let output: string = result.toString();
+  output = result === 0 ? '-' : `${Math.round(result * 100) / 100}%`;
+  if (result > 0) output = `+${output}`;
+  return output;
+};
+
+const PROCESSABLE_FIELDS = ['perf', 'a11y', 'bp', 'seo', 'average'];
+const PWA_FIELDS = ['fnr', 'ins', 'po'];
+
+const processResultAsDiff = (results: Result[], idx: number): void => {
+  /* eslint-disable security/detect-object-injection */
+  PROCESSABLE_FIELDS.forEach((key) => {
+    results[idx][key] =
+      parseFloat(results[idx][key] as string) - parseFloat(results[0][key] as string);
+    results[idx][key] = signsResult(results[idx][key] as number);
+  });
+  const refPwa = results[0].pwa as SubObj;
+  const pwa = results[idx].pwa as SubObj;
+  PWA_FIELDS.forEach((key) => {
+    pwa[key] = parseFloat(pwa[key] as string) - parseFloat(refPwa[key] as string);
+    pwa[key] = signsResult(pwa[key] as number);
+  });
+};
+
+const processDifference = (
+  scoreStrings: string[],
+  results: Result[],
+  asPercentage: boolean
+): Result[] => {
+  const ref = results[0];
+  const refPwa = ref.pwa as SubObj;
+  /* eslint-disable security/detect-object-injection */
+  if (asPercentage) {
+    for (let idx = 1; idx < scoreStrings.length; ++idx) {
+      processResultAsDiff(results, idx);
+    }
+    return results;
+  }
+  for (let idx = 1; idx < scoreStrings.length; ++idx) {
+    PROCESSABLE_FIELDS.forEach((key) => {
+      (results[idx][key] as number) -= ref[key] as number;
+    });
+    const pwa = results[idx].pwa as SubObj;
+    PWA_FIELDS.forEach((key) => {
+      (pwa[key] as number) -= refPwa[key] as number;
+    });
+  }
+  return results;
+};
+
+/**
+ * @private
+ */
+const perc = (num: number): string => `${Math.round(num * 10000) / 100}%`;
+
+const percentagify = (result: Result): Result => {
+  return {
+    perf: perc(result.perf as number),
+    a11y: perc(result.a11y as number),
+    bp: perc(result.bp as number),
+    seo: perc(result.seo as number),
+    pwa: {
+      fnr: perc(result.pwa.fnr as number),
+      ins: perc(result.pwa.ins as number),
+      po: perc(result.pwa.po as number)
+    },
+    average: perc(result.average as number)
+  };
+};
+
+/**
+ * @private
+ */
+const safeFractionEval = (fraction: string): number => {
+  // return fraction.split('/').map(parseFloat).reduce((acc, val) => acc / val);
+  const [num, denum] = fraction.split('/');
+  return parseInt(num) / parseInt(denum);
+};
+
+const perc2float = (percentages: string[], denominator = 100): number[] => {
+  return percentages.map((perc) => parseFloat(perc) / denominator);
+};
+
+const parseScoreString = (scoreStr: string): number[] => {
+  const { output, shorthandForm } = split(scoreStr);
+
+  const result = perc2float([output.perf, output.a11y, output.bp, output.seo]);
+  let fnr = null;
+  let ins = null;
+  let po = null;
+
+  if (shorthandForm) {
+    fnr = parseFloat(output.fnr) / 3;
+    ins = parseFloat(output.ins) / 3;
+    po = parseFloat(output.po) / 7;
+  } else {
+    fnr = safeFractionEval(output.fnr);
+    ins = safeFractionEval(output.ins);
+    po = safeFractionEval(output.po);
+  }
+  return result.concat(fnr, ins, po);
+};
+
+export { processDifference, percentagify, parseScoreString };
