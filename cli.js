3e3054028a7705e3abbb1ebe752fb147a07062ff
Berkmann18 <maxieberkmann@gmail.com>
2020-04-30 19:05:46 +0100

    feat(cli): configurations
    Added the possibility of using the CLI with no arguments and with a configuration file

--- cli.js
+++ cli.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
 const { program } = require('commander');
+const { cosmiconfig } = require('cosmiconfig');
 const pkg = require('./package.json');
 /* eslint-disable node/no-missing-require */
 const avg = require('./build/main').default;
@@ -12,9 +13,34 @@ const {
   textTransform
 } = require('./build/main/transform');
 
+const explorer = cosmiconfig(pkg.name);
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+const findConfig = () => {
+  const options = {
+    percentage: false,
+    format: 'text',
+    split: false,
+    diff: false,
+    names: [],
+    inputs: []
+  };
+  return new Promise((resolve, reject) => {
+    explorer
+      .search()
+      .then((result) => {
+        if (!result.isEmpty) {
+          resolve({ ...options, ...result.config });
+        }
+      })
+      .catch((err) => reject(err));
+  });
+};
+
 // TODO Refactor this to a shorter function with more re-usable code.
 program
-  .arguments('<scoreStrings...>')
+  .arguments('[scoreStrings...]')
   .version(pkg.version)
   .description(pkg.description)
   .option('-p, --percentage', 'Return the result(s) in percentage form')
@@ -25,25 +51,32 @@ program
   .option('-s, --split', 'Split the string by metric')
   .option('-d, --diff', 'Shows the difference between the first row and subsequent ones')
   .option('-n, --names <names>', 'Add names to each results', (value) => value.split(','))
-  .action((scoreStrings) => {
-    const results = avg(scoreStrings, {
-      asPercentage: program.percentage,
-      showDiff: program.diff,
-      names: program.names
+  .action(async (scoreStrings) => {
+    const options = await findConfig();
+    ['percentage', 'format', 'split', 'diff', 'names'].forEach((option) => {
+      // eslint-disable-next-line security/detect-object-injection
+      if (program[option]) options[option] = program[option];
+    });
+    if (scoreStrings.length) options.inputs = scoreStrings;
+
+    const results = avg(options.inputs, {
+      asPercentage: options.percentage,
+      showDiff: options.diff,
+      names: options.names
     });
 
     /* eslint-disable indent */
-    switch (program.format) {
+    switch (options.format) {
       case 'json':
-        return jsonTransform(results, program);
+        return jsonTransform(results, options);
       case 'csv':
-        return csvTransform(results, program, scoreStrings);
+        return csvTransform(results, options, options.inputs);
       case 'md':
-        return mdTransform(results, program, scoreStrings);
+        return mdTransform(results, options, options.inputs);
       case 'html':
-        return htmlTransform(results, program, scoreStrings);
+        return htmlTransform(results, options, options.inputs);
       default:
-        textTransform(results, program, scoreStrings);
+        textTransform(results, options, options.inputs);
     }
   })
   .parse(process.argv);
